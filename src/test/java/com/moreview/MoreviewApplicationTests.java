69de771166b2db40f085f33be2740873689e8ff9
jgmoon <6813821@tukorea.ac.kr>
2024-01-09 13:31:52 +0900

    Set up

---
+++ src/test/java/com/moreview/MoreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
