00090ef67489e33e5ae884ec6345d6f0febcafcd
jgmoon <6813821@tukorea.ac.kr>
2024-01-21 22:49:46 +0900

    주석 처리

--- src/main/java/com/moreview/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/moreview/global/exception/ApiExceptionHandler.java
@@ -1,21 +1,21 @@
-package com.moreview.global.exception;
-
-import io.swagger.v3.oas.models.responses.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ApiExceptionHandler {
-
-    @ExceptionHandler({ApiException.class})
-    public ResponseEntity<ApiExceptionResponse> handleApiException(final ApiException e) {
-        ErrorStatus errorStatus = e.getErrorStatus();
-        ApiExceptionResponse response = new ApiExceptionResponse(
-                errorStatus.getStatus().value(),
-                errorStatus.getErrorCode(),
-                errorStatus.getMessage()
-        );
-        return ResponseEntity.status(errorStatus.getStatus()).body(response);
-    }
-}
+//package com.moreview.global.exception;
+//
+//import io.swagger.v3.oas.models.responses.ApiResponse;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class ApiExceptionHandler {
+//
+//    @ExceptionHandler({ApiException.class})
+//    public ResponseEntity<ApiExceptionResponse> handleApiException(final ApiException e) {
+//        ErrorStatus errorStatus = e.getErrorStatus();
+//        ApiExceptionResponse response = new ApiExceptionResponse(
+//                errorStatus.getStatus().value(),
+//                errorStatus.getErrorCode(),
+//                errorStatus.getMessage()
+//        );
+//        return ResponseEntity.status(errorStatus.getStatus()).body(response);
+//    }
+//}
