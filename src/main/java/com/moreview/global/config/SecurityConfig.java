2846c91f21202d7e0774c13e56d563c332cd1aef
jgmoon <6813821@tukorea.ac.kr>
2024-02-27 01:30:51 +0900

    feat: Integrate CustomOAuth2UserService in SecurityConfig (#10)
    - CustomOAuth2UserService 인스턴스를 SecurityConfig 클래스에 주입하도록 수정하였습니다. 이를 통해 OAuth2 로그인 과정에서 서비스 별로 사용자 정보를 가져오는 로직을 커스텀할 수 있습니다.

--- src/main/java/com/moreview/global/config/SecurityConfig.java
+++ src/main/java/com/moreview/global/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.moreview.global.config;
 
 
+import com.moreview.global.security.Oauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,7 +37,9 @@ public class SecurityConfig {
 
         // oauth2
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)));
 
         // 경로별 인가 작업
         http
