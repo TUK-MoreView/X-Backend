371645d82ed9f0cd0877f0fc12deca44a143bc7c
jgmoon <6813821@tukorea.ac.kr>
2024-03-02 23:15:47 +0900

    feat: JWT 응답 메서드 생성 (#10)
    CustomSuccessHandler

---
+++ src/main/java/com/moreview/global/security/Oauth2/controller/CustomSuccessHandler.java
@@ -0,0 +1,39 @@
+package com.moreview.global.security.Oauth2.controller;
+
+import com.moreview.global.security.Oauth2.dto.CustomOAuth2User;
+import com.moreview.global.util.JwtUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+@Component
+@AllArgsConstructor
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        // OAuth2User
+        CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
+
+        String memberName = customUserDetails.getMemberName();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(memberName, role, 60 * 60 * 60L);
+    }
+}
