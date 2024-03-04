abbd822718b950b649cfed411b75533d06f095c4
jgmoon <6813821@tukorea.ac.kr>
2024-03-04 22:01:24 +0900

    fix: 컨트롤러 url 수정 (#10)
    AuthController

--- src/main/java/com/moreview/global/security/oauth2/controller/AuthController.java
+++ src/main/java/com/moreview/global/security/oauth2/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.moreview.global.security.oauth2.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RequiredArgsConstructor
+@RestController
 public class AuthController {
 
-    @GetMapping("/")
+    @GetMapping("/auth")
     @ResponseBody
     public String authAPI() {
 
