a5c371bbb5965f992e9d48f5a9f6bd4b1fe3c2ad
jgmoon <6813821@tukorea.ac.kr>
2024-03-19 23:49:47 +0900

    feat: S3 파일 업로드, 삭제 서비스 구현 (#10)
    S3FileUploader

--- src/main/java/com/moreview/global/s3/service/S3FileUploader.java
+++ src/main/java/com/moreview/global/s3/service/S3FileUploader.java
@@ -1,2 +1,50 @@
-package com.moreview.global.s3.service;public class S3FileUploader {
+package com.moreview.global.s3.service;
+
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.moreview.global.config.S3Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3FileUploader {
+
+    private final S3Config s3Config;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // 로컬환경경로
+    private String localLocation = "C:\\Users\\gmfd7\\OneDrive\\Desktop\\fil    e\\";
+
+    public String imageUpload(MultipartRequest request) throws IOException {
+
+        MultipartFile file = request.getFile("upload");
+
+        String fileName = file.getOriginalFilename();
+        String ext = fileName.substring(fileName.indexOf("."));
+
+        String uuidFileName = UUID.randomUUID() + ext;
+        String localPath = localLocation + uuidFileName;
+
+        File localFile = new File(localPath);
+        file.transferTo(localFile);
+
+
+        s3Config.amazonS3Client().putObject(new PutObjectRequest(bucket, uuidFileName, localFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        String s3Url = s3Config.amazonS3Client().getUrl(bucket, uuidFileName).toString();
+
+        localFile.delete();
+
+        return s3Url;
+
+    }
 }
