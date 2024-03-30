b2b6a260539be8c60f091cbb5bc1188c40a3250b
jgmoon <6813821@tukorea.ac.kr>
2024-03-31 05:04:37 +0900

    feat: 웹소켓 메시지 처리 로직 추가 (#13)
    WebSocketHandler

--- src/main/java/com/moreview/global/websocket/WebSocketHandler.java
+++ src/main/java/com/moreview/global/websocket/WebSocketHandler.java
@@ -1,8 +1,14 @@
 package com.moreview.global.websocket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moreview.domain.work.Service.WorkService;
+import com.moreview.domain.work.WorkSlide;
+import com.moreview.domain.work.WorkText;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
@@ -10,7 +16,26 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Slf4j
 public class WebSocketHandler extends TextWebSocketHandler {
 
-    private String postId;
+    private final WorkService workService;
+    private final ObjectMapper objectMapper;
+    private String workId;
+    private WorkSlide workSlide;
 
-    // todo 게시글의 자료들을 객체로 변환하는 로직
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        String payload = message.getPayload();
+
+        // 메시지를 WorkText 객체로 변환
+        WorkText workText = objectMapper.readValue(payload, WorkText.class);
+
+        // UUID 가져오기
+        workId = workText.getWorkId();
+
+        // WorkSlide 찾기
+        workSlide = workService.findWorkSlide(workId);
+
+        // 로직 실행
+        workService.handleMessage(workSlide, workText,session);
+    }
 }
